49df8d6e46f4383eefa0a4beef4fac8d4bfca3f2
VanessaLin9 <vanessa7591@gmail.com>
2022-01-28 06:47:41 +0800

    add GAME_STATE

---
+++ main.js
@@ -0,0 +1,7 @@
+//定義遊戲狀態
+const GAME_STATE ={
+  ShowPrizeAwaits: "ShowPrizeAwaits", //等待秀獎品
+  DrawLotsAwaits: "DrawLotsAwaits", //等待抽獎
+  ShowWinner: "ShowWinner", //增加得獎清單
+  GameFinished: "GameFinished", //完成抽獎
+}
