d6e0919f10a6d2685a71854041324769586d88cd
VanessaLin9 <vanessa7591@gmail.com>
2022-02-06 06:43:51 +0800

    add download feat

--- main.js
+++ main.js
@@ -7,8 +7,15 @@ const GAME_STATE ={
 }
 //獎品 & 參與者
  const prize = [
-  {"id":0,"name":"擠檸檬器","pictureURL":"https://i5.walmartimages.com/asr/b90ed4e5-d679-402c-ad70-771e0d13660a.e9a7becb3ab1ba138340d6a51148d57c.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},{"id":1,"name":"Amazon禮物卡$15","pictureURL":"https://gamecardsdirect.com/content/picture/23005/amazon-gift-card-15-dollar.jpg"},{"id":2,"name":"Amazon禮物卡$15","pictureURL":"https://gamecardsdirect.com/content/picture/23005/amazon-gift-card-15-dollar.jpg"},{"id":3,"name":"Amazon禮物卡$15","pictureURL":"https://gamecardsdirect.com/content/picture/23005/amazon-gift-card-15-dollar.jpg"},{"id":4,"name":"氣炸鍋","pictureURL":"https://slimages.macysassets.com/is/image/MCY/products/4/optimized/17998204_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$&fmt=webp"},{"id":5,"name":"刀組","pictureURL":"https://slimages.macysassets.com/is/image/MCY/products/2/optimized/16146892_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$&fmt=webp"},{"id":6,"name":"便當盒組","pictureURL":"https://m.media-amazon.com/images/I/81T0-lmYxPL._AC_SX522_.jpg"},{"id":7,"name":"果汁機","pictureURL":"https://slimages.macysassets.com/is/image/MCY/products/4/optimized/1786184_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$&fmt=webp"},{"id":8,"name":"Brita水壺","pictureURL":"https://m.media-amazon.com/images/I/61glxd8kQ5L._AC_SX679_.jpg"},{"id":9,"name":"Chromecast","pictureURL":"https://m.media-amazon.com/images/I/81rOtfzI0QL._AC_SX679_.jpg"},{"id":10,"name":"鬆餅機1","pictureURL":"https://slimages.macysassets.com/is/image/MCY/products/1/optimized/18195521_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$&fmt=webp"},{"id":11,"name":"鬆餅機2","pictureURL":"https://slimages.macysassets.com/is/image/MCY/products/8/optimized/19387718_fpx.tif?op_sharpen=1&wid=1230&hei=1500&fit=fit,1&$filterxlrg$&fmt=webp"},{"id":12,"name":"加濕器","pictureURL":"https://m.media-amazon.com/images/I/910iTpyL4GL._AC_SX522_.jpg"},{"id":13,"name":"行車紀錄器","pictureURL":"https://m.media-amazon.com/images/I/712VvxKgVHL._AC_SL1500_.jpg"},{"id":14,"name":"Oral-B電動牙刷","pictureURL":"https://m.media-amazon.com/images/I/81NKdvBMbHL._AC_SX679_.jpg"},{"id":15,"name":"Chromecast w/ Google TV","pictureURL":"https://i5.walmartimages.com/asr/a4e2e013-1d85-4380-8ace-e9780b074d54.1151d7079111b6ad4d9ef4e334953193.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"},{"id":16,"name":"Insignia 32\" TV","pictureURL":"https://m.media-amazon.com/images/I/51IvQZbr4hL._AC_.jpg"}
-]
+    {
+      "name": "膠囊咖啡機",
+      "pictureURL": "https://slimages.macysassets.com/is/image/MCY/products/4/optimized/20431034_fpx.tif?op_sharpen=1&amp;wid=700&amp;hei=855&amp;fit=fit,1" 
+    },
+    {
+      "name": "全套鍋具組",
+      "pictureURL": "https://imgur.com/a/O7IRvEL" 
+    }
+  ]
 const player = [
   {"id":0,"name":"UR_Father"},
   {"id":1,"name":"Tank"},
@@ -34,6 +41,7 @@ const decision = document.querySelector('.decision')
 const prizeFig = document.querySelector('#fig')
 const prizeName = document.querySelector('#prizeName')
 const winnerName = document.querySelector('#winnerName')
+const downloadBtn = document.querySelector('.download')
 
 //MVC架構
 
@@ -85,6 +93,9 @@ const view = {
   winnerName.innerHTML = "誰中獎!?"
   prizeName.innerHTML = ''
   prizeFig.src = "https://static.careerengine.us/api/aov2/https%3A_%7C__%7C_mmbiz.qpic.cn_%7C_mmbiz_gif_%7C_Fr2k3DYvg4cvChc1CjgvLtrmoYjhibAB42gmhAuUDiaVhfxAzZU7oiaACM2UuxuWOmqexJG49mTk1c8PFvsbBv27g_%7C_640%3Fwx_fmt%3Dgif"
+  },
+  finished() {
+    downloadBtn.style.display= 'flex'
   }
 }
 
@@ -121,25 +132,48 @@ const controller = {
         // 儲存清單
           model.winnerList.push(model.winnerIs[0])
           localStorage.setItem('winnerList',JSON.stringify(model.winnerList))
-          
+
         // 移除已中獎人
           prize.shift()
           model.winnerIs = [{}]
+
+        // 完成遊戲
           if (prize.length === 0){
             this.currentState = GAME_STATE.GameFinished
             console.log('抽完啦')
             console.log('清單', model.winnerList)
-            return
+            view.finished()
+            break
           }
         this.currentState = GAME_STATE.ShowPrizeAwaits
         break
-      }
+      } 
       break
+
+    case GAME_STATE.GameFinished:
+     if (control.id === "downloadBtn") {
+      console.log('yoyo')
+      model.download()
+      return
+    }
   }},
 }
 
 const model ={
   //和資料有關的程式碼
+  download(){
+    let data = JSON.stringify(model.winnerList)
+    let blob = new Blob([data], {
+    type: "application/octet-stream",
+    })
+    const fileName = "winnerList.cvs"
+    const href = URL.createObjectURL(blob)
+    const link = document.createElement("a")
+    document.body.appendChild(link)
+    link.href = href
+    link.download = fileName
+    link.click()
+  },
   removePlayer: '', 
   winnerList: [],
   winnerIs:[{}]
